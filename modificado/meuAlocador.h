63929466d97cc8127966ab347ef5b0a9cfad927c
guipateiro <57327655+guipateiro@users.noreply.github.com>
2022-04-26 14:11:26 -0300

    Add files via upload

---
+++ modificado/meuAlocador.h
@@ -0,0 +1,14 @@
+#ifndef __MEUALOCADOR2__
+#define __MEUALOCADOR2__
+
+extern void *topoInicialHeap;
+extern void *ponteiroAtual;
+
+void iniciaAlocador();
+void finalizaAlocador();
+void *alocaMem(long int);
+int liberaMem(void *);
+
+void imprimeMapa();
+
+#endif
